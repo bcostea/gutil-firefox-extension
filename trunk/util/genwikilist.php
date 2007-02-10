a5d47ae8e5ddaee0800e684ce8c194f79df6cd28
chtitux <chtitux@73972fbf-2d27-0410-b6f4-f15cd4d10e34>
2007-02-10 12:19:35 +0000

    * Add missing icons * Add clean buil.sh * Add french transaltion of GUtil! Options * Add logo in genwikilist.php

--- trunk/util/genwikilist.php
+++ trunk/util/genwikilist.php
@@ -1,5 +1,7 @@
 <?php 
 /* Warn : Gmail Link */
+
+/* http://gutil-firefox-extension.googlecode.com/svn/trunk/skin/img/ */
 include('csv2array.php'); 
 $str=file_get_contents('gutil_items.csv');
 $rows=CSV2Array($str);
@@ -8,10 +10,13 @@ $rows=CSV2Array($str);
        {
             if($rows[$i][0]=="---")
             {
-            }
-            else
+            } elseif ($rows[$i][3] == "runGMail();") {
+		echo "  # [https://gmail.com/ ".preg_replace("@chrome://gutil/@","http://gutil-firefox-extension.googlecode.com/svn/trunk/",$rows[$i][4])."] ".
+		"[https://gmail.com/ ".$rows[$i][2]."] (and hosted)\n";
+           } else
             {
-                echo "  # [".$rows[$i][3]." ".$rows[$i][2]."]\n";
+                echo "  # [".$rows[$i][3]." ".preg_replace("@chrome://gutil/@","http://gutil-firefox-extension.googlecode.com/svn/trunk/",$rows[$i][4])."] ".
+		"[".$rows[$i][3]." ".$rows[$i][2]."]\n";
             }
        }
 
