176fc49a4fd660979a8c110da44ed9cfe4a13b98
chtitux <chtitux@73972fbf-2d27-0410-b6f4-f15cd4d10e34>
2007-02-10 12:19:35 +0000

    * Add missing icons * Add clean buil.sh * Add french transaltion of GUtil! Options * Add logo in genwikilist.php

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm gutil.xpi
 mkdir -p build/chrome
-zip -0 -r build/chrome/gutil.jar locale content skin
+zip -0 -r build/chrome/gutil.jar `find locale/ -name "*.dtd"` `find content/ -name "*.js"` `find content/ -name "*.xul"` `find skin/ -name "*.png"` `find skin -name "*.css"`
 
 cp install.rdf build/install.rdf
 cp chrome.manifest build/chrome.manifest
